f9a6d8df913de12828b26f8047897a1a32a6049c
DenMitter <57758gfk@gmail.com>
2024-10-01 22:13:44 +0300

    edit migration

--- database/migrations/2024_09_28_162947_create_posts_table.php
+++ database/migrations/2024_09_28_162947_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100);
-            $table->string('contents', 1000);
+            $table->text('contents')->fulltext();
             $table->timestamps();
         });
     }
--- database/migrations/2024_10_01_101257_create_comments_table.php
+++ database/migrations/2024_10_01_101257_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->integer('post_id');
-            $table->string('contents');
+            $table->text('contents')->fulltext();
             $table->timestamps();
         });
     }
